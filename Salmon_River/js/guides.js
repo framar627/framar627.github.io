526694c990d7d3969eb6a492d80cfab21fa753b6
Frank Cosme <frank.cosme619@gmail.com>
2020-04-12 00:01:30 -0400

    guide

--- Salmon_River/js/guides.js
+++ Salmon_River/js/guides.js
@@ -1,76 +1,71 @@
-//debugger;
-const dataSourceURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-readTextFile("json/guides.json", function(text){
-    var data = JSON.parse(text);
-    console.log('DATA:', data);
-});
+init();
+    
+    function init() {
+        console.log("Iniciando el INIT");
+        /* genera_tabla(); */
+        loadJSON(function (response) {
+            // Parse JSON string into object
+            var actual_JSON = JSON.parse(response);
+            console.log({ actual_JSON });
+            const objetoGuides = actual_JSON['guides'];
+            console.log("Objeto Guides:", objetoGuides);
+            for (let i = 0; i < objetoGuides.length; i++) {
+                console.log(objetoGuides[i]);
+                document.getElementById('cont1').innerHTML = Object.values(objetoGuides[i]);
 
-fetch(dataSourceURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObj) {
-        const towns = jsonObj['guides'];
-        for (let i = 0; i < towns.length; i++) {
-            if (['Adrian Lasorte', 'James S. Zervos', 'Randy Jones'].indexOf(guides[i].name) > -1) {
-                let town = document.createElement('section');
-
-                if (guides[i].name == "Adrian Lasorte")
-                {
-                    guide.style.order = "1";
-                }
-                else if (guides[i].name == "James S.Zervos")
-                {
-                    guide.style.order = "2";
-                }
-                else if (guides[i].name == "Randy Jones")
-                {
-                    guide.style.order = "3";
-                }
-                          
-                let info = document.createElement('div');
-                let name = document.createElement('h2');
-                let email = document.createElement('h3');
-                let certificationLevel = document.createElement('div');
-                let yearsofExperience = document.createElement('div');
-                let image = document.createElement('img');
-                let biograpghy = document.createElement('div');
-
-                image.setAttribute('src', 'images/' + guides[i].photo);
-                image.setAttribute('alt', 'Image of ' + guides[i].name);
-                image.setAttribute('title', guides[i].name);
-                image.classList.add('image');
-                name.textContent = guides[i].name;
-                name.classList.add('guide');
-                email.textContent = guides[i].email;
-                email.classList.add('email');
-                yearsofExperience.textContent = 'Years of Experience: ' + guides[i].yearsofExperience;
-                yearsofExperience.classList.add('Years');
-                certificationLevel.textContent = 'Certification Level: ' + guides[i].certificationLevel;
-                certificationLevel.classList.add('certification');
-                biograpghy.textContent = 'Biography: ' + guides[i].biograpghy;
-                biograpghy.classList.add('Biography of');              
-                info.classList.add('info');
-                info.appendChild(name);
-                info.appendChild(email);
-                info.appendChild(yearsofExperience);
-                info.appendChild(certificationLevel);
-                info.appendChild(biograpghy);
-                info.appendChild(history);
-                town.appendChild(info);
-                town.appendChild(image);
-                document.querySelector('div.displayguides').appendChild(town);
             }
-        }
-    });
+            // Obtener la referencia del elemento body
+            var body = document.getElementsByTagName("body")[0];
+            // Crea un elemento <table> y un elemento <tbody>
+            var tabla = document.createElement("table");
+            var tblBody = document.createElement("tbody");
+            for (var i = 0; i < objetoGuides.length; i++) {
+                // Crea las hileras de la tabla
+                var hilera = document.createElement("tr");
+                for (var j = 0; j < 1; j++) {
+                    // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+                    // texto sea el contenido de <td>, ubica el elemento <td> al final
+                    // de la hilera de la tabla
+                    var celda = document.createElement("td");
+                    var name = document.createTextNode("name:" + objetoGuides[i].name +" ");
+                    var photo = document.createTextNode("photo:" + objetoGuides[i].photo +" ");
+                    var email = document.createTextNode("email:" + objetoGuides[i].email +" ");
+                    var certificationLevel = document.createTextNode("certificationLevel" + objetoGuides[i].certificationLevel +" ");
+                    var yearsofExperience = document.createTextNode("yearsofExperience: " + objetoGuides[i].yearsofExperience +" ");
+                    var events = document.createTextNode("events: " + Object.values(objetoGuides[i].events));
+                    celda.appendChild(name);
+                    celda.appendChild(photo);
+                    celda.appendChild(email);
+                    celda.appendChild(certificationLevel);
+                    celda.appendChild(yearsofExperience);
+                    celda.appendChild(events);
+                    hilera.appendChild(celda);
+                }
+                // agrega la hilera al final de la tabla (al final del elemento tblbody)
+                tblBody.appendChild(hilera);
+            }
+            // posiciona el <tbody> debajo del elemento <table>
+            tabla.appendChild(tblBody);
+            // appends <table> into <body>
+            body.appendChild(tabla);
+            // modifica el atributo "border" de la tabla y lo fija a "2";
+            tabla.setAttribute("border", "2");
+        });
+    }
+
+    function loadJSON(callback) {
+        /* debugger; */
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        /* xobj.open('GET', 'personas.json', true); */
+        xobj.open('GET', 'json/guides.json', true);
+        // Replace 'my_data' with the path to your file
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState === 4 && xobj.status === 200) {
+                // Required use of an anonymous callback 
+                // as .open() will NOT return a value but simply returns undefined in asynchronous mode
+                callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    }
